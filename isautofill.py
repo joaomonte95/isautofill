5ff84f4b236c23ad06bdfedd74a4c0021dc78659
João Victor Monte R. Tavares <jotavemonte@gmail.com>
2018-08-18 20:10:29 -0300

    changed object's intarface and fixed some bugs

--- isautofill.py
+++ isautofill.py
@@ -2,36 +2,36 @@ import requests
 
 class Isautofill:
 
-    def __init__(self,dev_key,region='br1'):        
+    def __init__(self,dev_key,region='br1'):
         self.region = region
         self.dev_key = dev_key
         self.common_path = f"https://{region}.api.riotgames.com"
-    
-    def get_summoner(self,summoner_name):
+
+    def _get_summoner(self,summoner_name):
         r = requests.get(f"{self.common_path}/lol/summoner/v3/summoners/by-name/{summoner_name}?api_key={self.dev_key}")
         return r.json()
 
-    def get_rank(self, summonerId):
+    def _get_rank(self, summonerId):
         r = requests.get(f"{self.common_path}/lol/league/v3/positions/by-summoner/{summonerId}?api_key={self.dev_key}")
         return r.json()
-    
-    def get_winrate(self, ranked_json):
+
+    def _get_winrate(self, ranked_json):
         wins = ranked_json[0]['wins']
         losses = ranked_json[0]['losses']
         return int((wins/(wins+losses))*100)
 
-    def get_nmatches(self, ranked_json,accountId):
+    def _get_nmatches(self, ranked_json,accountId):
         wins = ranked_json[0]['wins']
         losses = ranked_json[0]['losses']
         nranked = wins+losses
         if nranked <= 100:
             nranked = nranked
         else:
-            nranked = 100        
+            nranked = 100
         r = requests.get(f'{self.common_path}/lol/match/v3/matchlists/by-account/{accountId}?api_key={self.dev_key}',params={'beginIndex':0,'endIndex':nranked,'queue':[420]})
         return r.json()
-    
-    def get_mainrole(self, matches_json):
+
+    def _get_mainrole(self, matches_json):
         role_dic = {"MID":0,"DUO_SUPPORT":0,"DUO_CARRY":0,"DUO":0,"JUNGLE":0,"TOP":0,"NONE":0,"SOLO":0}
         for li in matches_json['matches']:
             if li['lane'] == "BOTTOM":
@@ -51,27 +51,28 @@ class Isautofill:
             elif v == max2:
                 maxkeyd[k] = v
         return maxkeyd
-    
-    def get_matchplayers(self,summonerId):
+
+    def _get_matchplayers(self,summonerId):
         r = requests.get(f'{self.common_path}/lol/spectator/v3/active-games/by-summoner/{summonerId}?api_key={self.dev_key}')
         data = r.json()
         sum_li = []
         for li in data['participants']:
             sum_li.append(li['summonerName'])
         return sum_li
-    
+
     def is_all_autofill(self,sum_name):
-        example = self.get_summoner(sum_name)
-        sum_li = self.get_matchplayers(example['id'])
+        subject = self._get_summoner(sum_name)
+        print(subject)
+        sum_li = self._get_matchplayers(subject["id"])
         for li in sum_li:
-            example = self.get_summoner(li)
-            rank_data = self.get_rank(example['id'])
-            nmatches = self.get_nmatches(rank_data,example['accountId'])
-            mainrole = self.get_mainrole(nmatches)
-            winrate = self.get_winrate(rank_data)
+            individual = self._get_summoner(li)
+            rank_data = self._get_rank(individual['id'])
+            nmatches = self._get_nmatches(rank_data,individual['accountId'])
+            mainrole = self._get_mainrole(nmatches)
+            winrate = self._get_winrate(rank_data)
             print(f'{li} || Main roles (role/games played): {mainrole} || Winrate: {winrate}')
 
 # example
-if __name__='__main__':
-    riot = Isautofill("dev key goes here")
-    riot.is_all_autofill('Summoner's name goes here')
+if __name__=='__main__':
+    riot = Isautofill("dev key here")
+    riot.is_all_autofill("summoner's name here")
